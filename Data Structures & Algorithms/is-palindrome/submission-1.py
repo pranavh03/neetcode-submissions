492effacaa600dc9091c5d9d3a0ce4e9930ea406
pranavh03 <76035476+pranavh03@users.noreply.github.com>
2026-05-21 10:41:46 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string=''
+        for i in range(len(s)):
+            if  s[i].isalnum() and s[i]!=' ':
+                string+=s[i]
+        print(string)                
+        for i,j in zip(range(0,len(string)),range(-1,-len(string),-1)):
+            if i==j :
+                True
+            if string[i].upper()!=string[j].upper():
+                return False
+        return True
+            
