bcfc22c7c571e4a179f8af16403f2fb5803d6e9d
pranavh03 <76035476+pranavh03@users.noreply.github.com>
2026-06-04 11:56:12 +0530

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    from collections import Counter
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen=defaultdict(list)
+        for word in strs:
+            key=''.join(sorted(word))
+            seen[key].append(word)
+        return [i for i in seen.values()]
+
+
+        
